740af3be7b9ed79a709a9de249483883830ba0cd
haqimdev <127011631+haqimdev@users.noreply.github.com>
2025-04-17 14:28:33 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,8 @@
 import pymysql.cursors
 from datetime import datetime
 from DB import DatabaseConnector
-import RPi.GPIO as GPIO  # Import the RPi.GPIO module
 import time  # Import the time module
-import gpiod
+
 from gpiozero import Button
 
 
@@ -11,36 +10,22 @@ class MachineStatusApp2:
     def __init__(self, master, temp):
         # Initialize
         self.master = master
-        self.temp = temp
+        self.temp = 0
         self.db = None
-        # GPIO.setmode(GPIO.BCM)  # Use BCM numbering
-        # GPIO.setwarnings(False)
-        # chip = gpiod.Chip('gpiochip4')
 
-    def setup_gpio(self, button_pin, time, status, machine_id):
-        # button_line = chip.get_line(button_pin)
-        # button_line.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
-        # self.button_pin = button_pin
-
-        # self.button_pin = 17  # GPIO pin number for the button
-        # GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Set up the button pin as input with pull-up resistor
-        # GPIO.setup(button_pin, GPIO.IN)  # Set up the button pin as input
-        # GPIO.add_event_detect(button_pin, GPIO.BOTH, callback=self.machine_callback, bouncetime=300)  # Add event detection
-        # button_state = button_line.get_value()
-
-        # if btn.is_pressed and self.temp == 0:
-        #     print("Button pressed")
-        #     self.start_run(time, status, machine_id)
-        #     self.temp = 1
-        # elif button_state == 0 and self.temp == 1:
-        #     print("Button released")
-        #     self.stop_run(time, status, machine_id)
-        #     self.temp = 0
+    def setup_gpio(self, button_pin,temp, time, status, machine_id):
+    # def setup_gpio(self, button_pin, temp):
         btn = Button(button_pin)
-        if btn.when_pressed:
-            print("Button pressed")
-        if btn.when_released:
-            print("Button Released")
+        if btn.is_pressed and temp == 0:
+            print(f"Button on pin {button_pin} is pressed")     
+            self.start_run(time, status, machine_id)
+            temp = 1
+        elif not btn.is_pressed and temp == 1:
+            print(f"Button on pin {button_pin} is not pressed")
+            self.stop_run(time, status, machine_id)
+            temp = 0
+        return temp
+        
 
     def connect(self):
         self.db = DatabaseConnector('172.20.10.2', 'root', 'IOtSt4ckToorMariaDb', 'sensor_data')
@@ -81,15 +66,22 @@ if __name__ == "__main__":
     app = MachineStatusApp2(None, None)
     app.connect()
     # app.dis()
+    rodi_green_temp = 0
+    rodi_red_temp = 0
+    rodi_yellow_temp = 0
+    lift_green_temp = 0
+    lift_red_temp = 0
+    lift_yellow_temp = 0
+
 
     if app.is_connected():
         try:
             while True:
-                print('Connected')
-                app.setup_gpio(21, datetime.now(), 'Green', 1)
-                app.setup_gpio(22, datetime.now(), 'Green', 2)
-        except KeyboardInterrupt:
-            # print('Diconnected')
-            GPIO.cleanup()
-            # button_line.release()
+                # print('Connected')
+                # app.setup_gpio(4, datetime.now(), 'Green', 1)
+                # app.setup_gpio(17, datetime.now(), 'Green', 2)               
+               rodi_green_temp=  app.setup_gpio(4,rodi_green_temp, datetime.now(), 'Green', 1)
+               lift_green_temp= app.setup_gpio(17, lift_green_temp, datetime.now(), 'Green', 2)
 
+        except KeyboardInterrupt:
+            print("\nProgram interrupted by user. Exiting...")
